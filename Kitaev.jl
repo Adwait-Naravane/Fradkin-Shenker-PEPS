511945d7c41eecfe9ec7bd4442c3da6b47b96c78
adwaitnaravane <adwaitnaravane@gmail.com>
2025-01-07 11:21:33 +0100

    Benchmarking Kitaev-Heisenberg ground states for AFM, FM and SL phases

--- Kitaev.jl
+++ Kitaev.jl
@@ -4,6 +4,8 @@ using TensorKit
 using KrylovKit
 using OptimKit
 using JLD2
+using LinearAlgebra
+BLAS.set_num_threads(20)
 
 function Kitaev_heisenberg(lattice::InfiniteSquare; kwargs...)
     return Kitaev_heisenberg(ComplexF64, Trivial, lattice; kwargs...)    
@@ -28,7 +30,7 @@ function Kitaev_heisenberg(T::Type{<:Number},
 
 end
 
-H = Kitaev_heisenberg(InfiniteSquare(2,2); ϕ=0, h=0)
+H = Kitaev_heisenberg(InfiniteSquare(2,2); ϕ=90, h=0)
 
 D = 4
 χ = 24
@@ -49,10 +51,14 @@ env_init = leading_boundary(CTMRGEnv(Ψ, ComplexSpace(χ)), Ψ, ctm_alg);
 
 result = fixedpoint(Ψ, H, opt_alg, env_init)
 
+<<<<<<< HEAD
 file = jldopen("Kitaev_heisenberg_D=4_chi=24_ABBA_phi=0_h=0.jld2", "w")
+=======
+file = jldopen("Kitaev_heisenberg_D=4_chi=24_ABBA_phi=90_h=0_correct.jld2", "w")
+>>>>>>> 1f2609a (Benchmarking Kitaev-Heisenberg ground states for AFM, FM and SL phases)
 file["result"] = result
 close(file)
 
-file = jldopen("Kitaev_heisenberg_phi=180_h=0.jld2", "r")
+file = jldopen("Kitaev_heisenberg_D=4_chi=24_ABBA_phi=90_h=0_correct.jld2", "r")
 result = file["result"]
 close(file)
