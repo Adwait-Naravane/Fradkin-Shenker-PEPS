d4a8af3f6d7806ab80c852aeb4bae5d99ca80f55
vsc48647 vscuser <vsc48647@gligar09.gastly.os>
2025-05-20 15:58:58 +0200

    New submit file

--- submit.sh
+++ submit.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # Define groups of (hx, hz) points
-group1_hx=($(seq 0.2 0.01 0.5))
-group1_hz=(0.0)
+# group1_hx=($(seq 0.25 0.01 0.45))
+# group1_hz=(0.0)
 
-group2_hx=(0.0)
-group2_hz=($(seq 0.2 0.01 0.5))
+# group2_hx=(0.0)
+# group2_hz=($(seq 0.25 0.01 0.45))
 
-group3_hx=($(seq 0.25 0.05 0.4))
-group3_hz=(0.2)
+group3_hx=($(seq 0.25 0.005 0.4))
+group3_hz=(0.21)
 
-group4_hx=(0.2)
-group4_hz=($(seq 0.25 0.05 0.4))
+group4_hx=(0.21)
+group4_hz=($(seq 0.25 0.005 0.4))
 
-# All combinations
+# Build combined hx/hz pairs
 hx_list=()
 hz_list=()
 
-for h in "${group1_hx[@]}"; do
-  hx_list+=("$h")
-  hz_list+=("0.0")
-done
+# for h in "${group1_hx[@]}"; do
+#   hx_list+=("$h")
+#   hz_list+=("0.0")
+# done
 
-for h in "${group2_hz[@]}"; do
-  hx_list+=("0.0")
-  hz_list+=("$h")
-done
+# for h in "${group2_hz[@]}"; do
+#   hx_list+=("0.0")
+#   hz_list+=("$h")
+# done
 
 for h in "${group3_hx[@]}"; do
   hx_list+=("$h")
@@ -38,19 +38,18 @@ for h in "${group4_hz[@]}"; do
 done
 
 # Chi and D values
-chi_vals=(12 16 24 36)
-D_vals=(4)
+chi_vals=(16 24 36)
+D_vals=(4 6)
 
-# Ensure log folder exists
+# Ensure logs directory exists
 mkdir -p pbs_logs
 
-# Submit jobs for all combinations
-for ((i=0; i<${#hx_list[@]}; i++)); do
-  hx=${hx_list[$i]}
-  hz=${hz_list[$i]}
-
+# Outer loop over D and chi
+for D in "${D_vals[@]}"; do
   for chi in "${chi_vals[@]}"; do
-    for D in "${D_vals[@]}"; do
+    for ((i=0; i<${#hx_list[@]}; i++)); do
+      hx=${hx_list[$i]}
+      hz=${hz_list[$i]}
 
       JOBNAME="Gauge-D${D}-chi${chi}-hx${hx}-hz${hz}"
       FILENAME="pbs_job_${hx}_${hz}_chi${chi}_D${D}.pbs"
